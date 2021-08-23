5a2c338f400262026d30e83ecceaabb5b5922ab4
Adrian Sian <adriansian@gmail.com>
2021-08-23 16:05:38 +0800

    myhub: add gay stuff
    Signed-off-by: Adrian Sian <adriansian@gmail.com>

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,8 +4,8 @@ export default class Navbar extends Component {
     render() {
         return (
             <div className="w-screen flex flex-nowrap justify-between items-center py-2 bg-ub-grey text-ubt-grey">
-                <span>Activities</span>
-                <span>Fri Mar 5 11:00 PM</span>
+                <span>Whatever</span>
+                <span>Mon Aug 23 11:00 PM</span>
                 <div className="wsb">
                     <span>w</span>
                     <span>s</span>
