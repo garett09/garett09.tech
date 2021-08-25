ade2c56404d2ccc0ab03c687c9504c42e22a0cb5
Adrian <adriansian@gmail.com>
2021-08-25 13:23:27 +0800

    myhub: update the syntax to use camel case

--- src/components/util components/clock.js
+++ src/components/util components/clock.js
@@ -3,7 +3,7 @@ import { Component } from "react";
 export default class Clock extends Component {
   constructor() {
     super();
-    this.month_list = [
+    this.monthList = [
       "Jan",
       "Feb",
       "Mar",
@@ -17,36 +17,36 @@ export default class Clock extends Component {
       "Nov",
       "Dec",
     ];
-    this.day_list = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    this.dayList = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     this.state = {
       hour_12: true,
-      current_time: new Date(),
+      currentTime: new Date(),
     };
   }
 
   componentDidMount() {
-    this.update_time = setInterval(() => {
-      this.setState({ current_time: new Date() });
+    this.updateTime = setInterval(() => {
+      this.setState({ currentTime: new Date() });
     }, 1 * 1000);
   }
 
   componentWillUnmount() {
-    clearInterval(this.update_time);
+    clearInterval(this.updateTime);
   }
 
   render() {
-    const { current_time } = this.state;
+    const { currentTime } = this.state;
 
-    let day = this.day_list[current_time.getDay()];
-    let hour = current_time.getHours();
-    let minute = current_time.getMinutes();
-    let month = this.month_list[current_time.getMonth()];
-    let date = current_time.getDate().toLocaleString();
+    let day = this.dayList[currentTime.getDay()];
+    let hour = currentTime.getHours();
+    let minute = currentTime.getMinutes();
+    let month = this.monthList[currentTime.getMonth()];
+    let date = currentTime.getDate().toLocaleString();
     let meridiem = hour < 12 ? "AM" : "PM";
 
     if (this.state.hour_12 && hour > 12) hour -= 12;
 
-    let display_time =
+    let displayTime =
       day +
       " " +
       month +
@@ -59,6 +59,6 @@ export default class Clock extends Component {
       " " +
       meridiem;
 
-    return display_time;
+    return displayTime;
   }
 }
