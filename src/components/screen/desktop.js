f21410d5b53aec596ff3636f9f0d5f1dbb38c504
Adrian Sian <adriansian@gmail.com>
2021-08-28 22:28:14 +0800

    myhub: added onclick window's focus
    Signed-off-by: Adrian Sian <adriansian@gmail.com>
    Reviewed-by: Adrian Sian <adriansian@gmail.com>
    Change-Id: Idcb72170fde014630ad522b0f5633146c8ee8c9b

--- src/components/screen/desktop.js
+++ src/components/screen/desktop.js
@@ -6,21 +6,35 @@ export class Desktop extends Component {
     constructor() {
         super();
         this.state = {
+            focused_windows: {
+                "chrome_0": true,
+                "chrome_1": false,
+            }
         }
     }
 
 
     focus = (objId) => {
-
+        var focused_windows = this.state.focused_windows;
+        focused_windows[objId] = true;
+        for (let key in focused_windows) {
+            if (focused_windows.hasOwnProperty(key)) {
+                if (key !== objId) {
+                    focused_windows[key] = false;
+                }
+            }
+        }
+        this.setState({ focused_windows });
     }
+
     render() {
         return (
             <div className="h-full w-full pt-6 bg-transparent overflow-hidden overscroll-none window-parent">
                 <BackgroundImage img={this.props.bg_img_path} />
 
                 {/* All Apps */}
-                <Window title="Google Chrome" id="chrome_0" focus={this.focus} isFocused={true} />
-                <Window title="Google Chrome1" id="chrome_1" focus={this.focus} isFocused={false} />
+                <Window title="Google Chrome" id="chrome_0" focus={this.focus} isFocused={this.state.focused_windows["chrome_0"]} />
+                <Window title="Hmmm.... Testing my penis is big lol" id="chrome_1" focus={this.focus} isFocused={this.state.focused_windows["chrome_1"]} />
 
             </div>
         )
